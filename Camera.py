be8ef2d671d519d3a0858c607710bd07be2a4969
Seighart <ShenTheDiego@users.noreply.github.com>
2026-02-24 19:04:12 +0900

    feat: add initial Rubik detection pipeline

---
+++ Camera.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import numpy
+from grid import draw_3x3_grid
+
+
+print("OpenCV version:", cv.__version__)
+
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        # print(ret)#     Boolean (True / False) •	True → frame successfully captured , False → failed to capture frame
+        # print(frame) # (height, width, channels)
+        break
+    #frame, boxes = draw_3x3_grid(frame)  # <-- overlay grid
+    # Flip the frame horizontally
+    frame = cv.flip(frame, 1)
+    # (frame) can stack modifier like cv.flip, cv.resize, etc. to modify the frame before displaying it.
+
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # Convert the frame to grayscale
+    blurred = cv.GaussianBlur(gray, (3, 3), 0)
+    canny = cv.Canny(blurred, 10, 50)
+
+#contours, hierarchy = cv2.findContours(image, mode, method[, offset])
+    contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    #cv.drawContours(frame, contours, -1, (0,255,0), 3)
+
+
+    
+    #cv.imshow("Gray", gray)  # show the grayscale image in a window named "Gray"
+    # cv.imshow("Blurred", blurred)  
+    #cv.imshow("Canny Edges", canny)  # show the canny edges in a window named "Canny Edges"
+    #cv.imshow("Camera", frame)  # show the camera feed in a window named "Camera"
+    cv.imshow("Camera", frame)
+
+    if cv.waitKey(1) & 0xFF == ord("q"):  # Stop the camera when the user presses the 'q' key
+        break
+
+
---
+++ grid.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+import cv2 as cv
+
+def draw_3x3_grid(Grid_frame, cell=200, gap=10, thickness=2):
+    """
+    Draw a centered 3x3 grid on the frame.
+    Returns: (frame, boxes)
+    boxes = [(x1,y1,x2,y2), ...] 9 items, row-major order
+    """
+    h, w = Grid_frame.shape[:2]
+
+    grid_size = 3 * cell + 2 * gap
+    start_x = w // 2 - grid_size // 2
+    start_y = h // 2 - grid_size // 2
+
+    boxes = []
+    for r in range(3):
+        for c in range(3):
+            x1 = start_x + c * (cell + gap)
+            y1 = start_y + r * (cell + gap)
+            x2 = x1 + cell
+            y2 = y1 + cell
+
+            cv.rectangle(Grid_frame, (x1, y1), (x2, y2), (0, 255, 0), thickness)
+            boxes.append((x1, y1, x2, y2))
+
+    return Grid_frame, boxes
