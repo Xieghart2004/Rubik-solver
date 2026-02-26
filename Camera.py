2ce09d90861d9d16f8bcafa733328617644f4a87
Seighart <ShenTheDiego@users.noreply.github.com>
2026-02-26 17:22:43 +0900

    feat: fixed a flip bugs (fliped camera and store value)

--- Camera.py
+++ Camera.py
@@ -4,9 +4,40 @@ from grid import draw_3x3_grid
 import kociemba
 from collections import Counter
 
-print("OpenCV version:", cv.__version__)
-print("press u r f d l b → store faces")
-print("FACE Green to the camera ")
+print("OpenCV version:", cv.__version__ + "\n")
+print("=== RUBIK'S CUBE CAPTURE GUIDE ===\n")
+print("Press U R F D L B to store each face")
+
+
+print("1) Hold the GREEN face toward the camera.")
+print("   Make sure the WHITE face is on TOP.")
+print("   Press 'F' to store.\n")
+
+print("2) Rotate the cube to the RIGHT.")
+print("   The RED face should now face the camera.")
+print("   Keep the WHITE face on TOP.")
+print("   Press 'R' to store.\n")
+
+print("3) Rotate the cube to the RIGHT again.")
+print("   The BLUE face should now face the camera.")
+print("   Keep the WHITE face on TOP.")
+print("   Press 'B' to store.\n")
+
+print("4) Rotate the cube to the RIGHT again.")
+print("   The ORANGE face should now face the camera.")
+print("   Keep the WHITE face on TOP.")
+print("   Press 'L' to store.\n")
+
+print("5) Turn The GREEN side faces the camera again.")
+print("   Then rotate the cube UP so WHITE faces the camera.")
+print("   Press 'U' to store.\n")
+
+print("6) Rotate the cube DOWN so YELLOW faces the camera.")
+print("   Press 'D' to store.\n")
+
+print("After all 6 faces are stored, press 'S' to solve. \n")
+print("==================================== GOOD LUCK! ====================================\n")
+
 
 cap = cv.VideoCapture(0)
 #I just know that the origin of cordinate is at the top-left corner. lol :O
@@ -88,6 +119,14 @@ def classify_cell_hsv(cell_bgr, ranges_dict):
 
     return best_name, best_score
 
+def unmirror_face(face9):
+    # face9 is [0..8] row-major.
+    # Mirror fix = reverse each row: (0,1,2)->(2,1,0), (3,4,5)->(5,4,3), (6,7,8)->(8,7,6)
+    return [
+        face9[2], face9[1], face9[0],
+        face9[5], face9[4], face9[3],
+        face9[8], face9[7], face9[6],
+    ]
 def show_face(face_key):
     s = face_to_str(faces[face_key])
     print(face_key)
@@ -102,19 +141,13 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
     if not ret:
-        # print(ret)#     Boolean (True / False) •	True → frame successfully captured , False → failed to capture frame
-        # print(frame) # (height, width, channels)
         break
-    # Flip the frame horizontally
-    frame = cv.flip(frame, 1)
-
+    display = cv.flip(frame, 1) # flip the frame horizontally for a mirror-like display
     
-    frame, boxes = draw_3x3_grid(frame)  # <-- overlay grid 
+    frame, boxes = draw_3x3_grid(display)  # <-- overlay grid 
     # (frame) can stack modifier like cv.flip, cv.resize, etc. to modify the frame before displaying it.
 
     detected_colors = []
-
-    
 
     for idx, (x1, y1, x2, y2) in enumerate(boxes):
         #(0, boxes[0]). (1, boxes[1]). (2, boxes[2])
@@ -132,7 +165,7 @@ while True:
         cy = (y1 + y2) // 2
 
         cv.putText(
-            frame,
+            display,
             letter,
             (cx - 10, cy + 10),
             cv.FONT_HERSHEY_SIMPLEX,
@@ -142,61 +175,62 @@ while True:
             cv.LINE_AA
         )
         detected_colors.append(color_name)
-        #print(f"Grid {idx} → {color_name}")
-    #---------------
-    # my red and orage ranges are not good enough, so I want to see the average hue of the cell to adjust the ranges.
-    #---------------
-    # hsv_cell = cv.cvtColor(cell, cv.COLOR_BGR2HSV)
-    # avg_h = np.mean(hsv_cell[:,:,0]) 
-    # print("Average Hue:", avg_h)
 
     left   = boxes[0][0]   # x1 of top-left (650)
     top    = boxes[0][1]   # y1 of top-left (230)
     right  = boxes[8][2]   # x2 of bottom-right (1270)
     bottom = boxes[8][3]   # y2 of bottom-right (850)
 
-    roi = frame[top:bottom, left:right]
-    gray = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)  # Convert the frame to grayscale
-    #hsvFrame = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-    blurred = cv.GaussianBlur(gray, (3, 3), 0)
-    canny = cv.Canny(blurred, 10, 50)
-    contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow("Camera", display)
 
-    cv.imshow("Camera", frame)
+
+    roi = frame[top:bottom, left:right]
+    
     key = cv.waitKey(1) & 0xFF
 
     # --- store faces ---
     if key == ord('u'):
-        faces["U"] = detected_colors.copy()
-        print("Stored U face")
-        print("White side's value:", faces["U"])
+        faces["U"] = unmirror_face(detected_colors)
+        print("Stored Upper face\n")
+        print("Rotate the cube to down so the YELLOW center faces the camera make sure that the orange side is on the right and then press 'D'.")
+        #print("White side's value:", faces["U"])
     elif key == ord('r'):
-        faces["R"] = detected_colors.copy()
-        print("Stored R face")
-        print("Red side's value:", faces["R"])
+        faces["R"] = unmirror_face(detected_colors)
+        print("Stored Right face\n")
+        print("Rotate the cube to the right so the BLUE center faces the camera make sure that WHITE stays on top, then press 'B'.")
+        #print("Red side's value:", faces["R"])
     elif key == ord('f'):
-        faces["F"] = detected_colors.copy()
-        print("Stored F face")
-        print("Green side's value:", faces["F"])
+        faces["F"] = unmirror_face(detected_colors)
+        print("Stored Front face\n")
+        print("Rotate the cube to the right so the RED center faces the camera and WHITE stays on top, then press 'R'.")
+        
+        #print("Green side's value:", faces["F"])
     elif key == ord('d'):
-        faces["D"] = detected_colors.copy()
-        print("Stored D face")
-        print("Yellow side's value:", faces["D"])
+        faces["D"] = unmirror_face(detected_colors)
+        print("Stored Down face\n")
+        print("Press 'S' to solve the cube.")
+        #print("Stored D face")
+        #print("Yellow side's value:", faces["D"])
     elif key == ord('l'):
-        faces["L"] = detected_colors.copy()
-        print("Stored L face")
-        print("Orange side's value:", faces["L"])
+        faces["L"] = unmirror_face(detected_colors)
+        print("Stored Left face\n")
+        print("Rotate the cube to the right so the GREEN center faces the camera and make sure that white face is still on top, Then rotate the cube up so WHITE faces the camera then press 'U'.")
+        #print("Stored L face")
+        #print("Orange side's value:", faces["L"])
     elif key == ord('b'):
-        faces["B"] = detected_colors.copy()
-        print("Stored B face")
-        print("Blue side's value:", faces["B"])
+        faces["B"] = unmirror_face(detected_colors)
+        print("Stored Back face\n")
+        print("Rotate the cube to the right so the ORANGE center faces the camera and WHITE stays on top, then press 'L'.")
+        #print("Stored B face")
+        #print("Blue side's value:", faces["B"])
 
     elif key == ord('q'):
         break
     
 # --- solve when all faces exist (optional: trigger on 's' instead) ---
     elif key == ord('s'):
-        
+        for k in ["U","R","F","D","L","B"]:
+            show_face(k)
         if not all(v is not None for v in faces.values()):
             print("Not all faces captured yet!")
             missing = [k for k, v in faces.items() if v is None]
